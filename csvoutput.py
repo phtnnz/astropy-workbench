a51b83d5656eedd6bfcdb9bfb28c910f2ba24044
phtnnz <git16@mj.m-j-s.net>
2024-07-13 10:33:20 +0200

    Functional complete including locale support

--- csvoutput.py
+++ csvoutput.py
@@ -14,12 +14,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Usage
+#   from csvoutput import CSVOutput
+#   CSVOutput.set_default_locale(locale="")
+#   CSVOutput.set_float_format(fmt="%.3f")
+#   CSVOutput.add_row(list)
+#   CSVOutput.add_fields(list)
+#   CSVOutput.write(file="", set_locale=True)       file="" uses stdout
+
 # ChangeLog
 # Version 0.0 / 2024-07-12
 #       locale aware global CSV output class
 
+
 import csv
 import locale
+import sys
 
 VERSION = "0.0 / 2024-07-12"
 AUTHOR  = "Martin Junius"
@@ -27,34 +37,60 @@ NAME    = "csvoutput"
 
 
 
+DEFAULT_FLOAT_FORMAT = "%.3f"
+
 class CSVOutput:
-    _cache  = []
-    _fields = None
-    _locale = None
+    _cache      = []
+    _fields     = None
+    _float_fmt  = None    # format for floats if set
 
-    def set_default_locale():
-        locale.setlocale(locale.LC_ALL, "")
 
-    def add_csv_row(data):
+    def set_default_locale(loc=""):
+        locale.setlocale(locale.LC_ALL, loc)
+        if locale.localeconv()['decimal_point'] == ",":
+            # Set float format to automatically format all float values as strings using locale
+            if not CSVOutput._float_fmt:
+                CSVOutput.set_float_format()
+
+
+    def set_float_format(fmt=DEFAULT_FLOAT_FORMAT):
+        CSVOutput._float_fmt = fmt
+
+
+    def _fmt(v):
+        return locale.format_string(CSVOutput._float_fmt, v)
+
+
+    def add_row(data):
         CSVOutput._cache.append(data)
 
-    def add_csv_fields(fields):
+
+    def add_fields(fields):
         CSVOutput._fields = fields
 
-    def write_csv(file):
-        with open(file, 'w', newline='', encoding="utf-8") as f:
-            ##FIXME: check  locale.RADIXCHAR
-            if locale.localeconv()['decimal_point'] == ",":
-                # Use ; as the separator and quote all fields for easy import in "German" Excel
-                writer = csv.writer(f, dialect="excel", delimiter=";", quoting=csv.QUOTE_ALL)
-            else:
-                writer = csv.writer(f, dialect="excel")
-            if CSVOutput._fields:
-                writer.writerow(CSVOutput._fields)
-            writer.writerows(CSVOutput._cache)
+
+    def _write(f):            
+        if locale.localeconv()['decimal_point'] == ",":
+            # Use ; as the separator and quote all fields for easy import in "German" Excel
+            writer = csv.writer(f, dialect="excel", delimiter=";", quoting=csv.QUOTE_ALL)
+        else:
+            writer = csv.writer(f, dialect="excel")
+        if CSVOutput._fields:
+            writer.writerow(CSVOutput._fields)
+        
+        # Write rows one by one the enable conversion of float values
+        for row in CSVOutput._cache:
+            if CSVOutput._float_fmt:
+                row = [ CSVOutput._fmt(v) if type(v) == float else v   for v in row ]
+            writer.writerow(row)
 
 
+    def write(file=None, set_locale=True):
+        if set_locale:
+            CSVOutput.set_default_locale()
 
-def val(val):
-    return locale.format_string("%.3f", val)
-
+        if file:
+            with open(file, 'w', newline='', encoding="utf-8") as f:
+                CSVOutput._write(f)
+        else:
+                CSVOutput._write(sys.stdout)
