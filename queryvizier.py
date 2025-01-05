d5746090561b8cb6007c18974ed1f665a780895a
phtnnz <git16@mj.m-j-s.net>
2025-01-05 22:52:28 +0100

    Added substitution scheme TABLE=OUT for columns, added CSV output

--- queryvizier.py
+++ queryvizier.py
@@ -36,6 +36,7 @@ from astroquery.vizier import Vizier
 
 # Local modules
 from verbose import verbose, warning, error
+from csvoutput import csv_output
 
 VERSION = "0.1 / 2025-01-05"
 AUTHOR  = "Martin Junius"
@@ -44,26 +45,35 @@ NAME    = "queryvizier"
 
 
 def query_vizier(cat: str, cols: list=None, row_limit: int=-1):
-    # vizier = Vizier(catalog=cat)
+    # Process TABLE=OUT in cols list
+    if cols:
+        table_cols = [ col.split("=")[0] if "=" in col else col for col in cols ]
+        out_cols   = [ col.split("=")[1] if "=" in col else col for col in cols ]
+        ic(table_cols, out_cols)
+
+    # Query VizieR
     vizier = Vizier(catalog=cat,
-                    columns=["**"],        # "*" = default columns, "**" = all columns
+                    columns=["**"],                     # "*" = default columns, "**" = all columns
                     row_limit = row_limit
                    )
     meta = vizier.get_catalog_metadata()
     ic(vizier, meta)
-    result = vizier.query_object("")                # "" = query all catalog entries
+    result = vizier.query_object("")                    # "" = query all catalog entries
     ic(result)
     for name in result.keys():
         table = result[name]
         if not cols:
-            cols = table.keys()
-        ic(table, cols)
+            table_cols = out_cols = table.keys()
+        ic(table, table_cols, out_cols)
+        verbose(f"table columns = {table_cols}")
+        verbose(f"output columns = {out_cols}")
+        csv_output(fields=out_cols)
         for row in table:
             ic(row)
-            # values = list(row)                    # Convert row to list
-            values = [ row[col] for col in cols ]   # Selected columns
+            # values = list(row)                        # Convert row to list
+            values = [ row[col] for col in table_cols ] # Selected columns
             ic(values)
-    pass
+            csv_output(row=values)
 
 
 
@@ -76,6 +86,9 @@ def main():
     arg.add_argument("-d", "--debug", action="store_true", help="more debug messages")
     arg.add_argument("--columns", help="columns to retrieve, comma-separated, default all")
     arg.add_argument("--row-limit", help="number of rows to retrieve, default unlimited")
+    arg.add_argument("-C", "--csv", action="store_true", help="CSV output")
+    arg.add_argument("-o", "--output", help="output file")
+
     arg.add_argument("catalog", help="catalog name")
 
     args = arg.parse_args()
@@ -86,16 +99,18 @@ def main():
     if args.verbose:
         verbose.set_prog(NAME)
         verbose.enable()
+
     cat = args.catalog
     columns = None
     row_limit = int(args.row_limit or -1)
     if args.columns:
         columns = args.columns.split(",")
+
     verbose(f"query catalog {cat}, {row_limit=}")
-    if columns:
-        verbose(f"columns = {columns}")
     query_vizier(cat, columns, row_limit)
 
+    if args.csv:
+        csv_output.write(args.output, set_locale=False)
 
 
 if __name__ == "__main__":
