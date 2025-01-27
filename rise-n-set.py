621a21c46f323006e99c503366284ce0d6c1c984
phtnnz <git16@mj.m-j-s.net>
2025-01-27 22:21:29 +0100

    Fixed height for default location

--- rise-n-set.py
+++ rise-n-set.py
@@ -76,7 +76,7 @@ def main():
         verbose.enable()
 
     # Default location is Hakos, Namibia for personal reasons ;-)
-    loc      = EarthLocation(lon=16.36167*u.deg , lat=-23.23639*u.deg, height=0*u.m)
+    loc      = EarthLocation(lon=16.36167*u.deg , lat=-23.23639*u.deg, height=1853*u.m)
     if args.location:
         loc = get_location(args.location)
     ic(loc, loc.to_geodetic())
