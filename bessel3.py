5ce614a7c6164f1c55f9ea96343bc010d6567ab1
phtnnz <git16@mj.m-j-s.net>
2025-10-19 16:10:48 +0200

    More renaming, use EarthLocation x,y,z to compute geocentric coordinates

--- bessel3.py
+++ bessel3.py
@@ -50,6 +50,18 @@ DEFAULT_LOCATION = "Burgos, Spain"
 
 
 
+# Earth equatorial radius
+R_earth = 6378137.0 * u.m           # GRS 80/WGS 84 value (Wikipedia)
+                                    # https://en.wikipedia.org/wiki/World_Geodetic_System
+R_earth_pol = 6356752.0 * u.m       # https://en.wikipedia.org/wiki/Earth
+f_earth = (R_earth - R_earth_pol) / R_earth
+# Moon equatorial radius
+R_moon = 1738100.0 * u.m
+R_moon = 0.2725076 * R_earth        # IAU 1982
+R_moon = 0.272281  * R_earth        # smaller value from https://eclipse.gsfc.nasa.gov/SEpubs/20080801/TP214149b.pdf
+
+
+
 # Command line options
 class Options:
     loc: EarthLocation = None       # -l --location
@@ -106,7 +118,7 @@ def test_tse2026() -> Tuple[EarthLocation, Time]:
 # µ      - Hour angle of the Moon's shadow axis on the celestial sphere
 # f1, f2 - Angles of the penumbral and umbral/antumbral shadow cones with respect to the axis of the lunar shadow
 
-T0 = 18
+bessel_T0 = 18
 bessel_x       = [  0.4755140,  0.5189249, -0.0000773, -0.0000080 ]
 bessel_y       = [  0.7711830, -0.2301680, -0.0001246,  0.0000038 ]
 bessel_d       = [ 14.7966700, -0.0120650, -0.0000030,  0.0       ]
@@ -121,7 +133,7 @@ polynomial_y   = Polynomial(bessel_y)
 polynomial_d   = Polynomial(bessel_d)
 polynomial_l1  = Polynomial(bessel_l1)
 polynomial_l2  = Polynomial(bessel_l2)
-polynomial_u   = Polynomial(bessel_u)
+polynomial_u   = Polynomial(bessel_u) 
 
 polynomial_x_p = polynomial_x.deriv()
 polynomial_y_p = polynomial_y.deriv()
@@ -135,10 +147,13 @@ def atan2(y, x):    return np.rad2deg( np.atan2(y, x) )
 def asin(x):        return np.rad2deg( np.asin(x) )
 def acos(x):        return np.rad2deg( np.acos(x) )
 def atan(x):        return np.rad2deg( np.atan(x) )
+def sqrt(x):        return np.sqrt(x)
+def sq(x):          return x*x
+
 
 
 # Ergebnisse der Fundamentalebene für einen Zeitpunkt
-def magnitudePoswinkel(t: float, pSinPS: float, pCosPS: float, longitude: float, delta_t: float) -> Tuple[float, float, float, float, float, float, float, float, float]:
+def calc_fundamental_plane(t: float, rho_sin_phi_p: float, rho_cos_phi_p: float, longitude: float, delta_t: float) -> Tuple[float, float, float, float, float, float, float, float, float]:
     X  = polynomial_x(t)
     Y  = polynomial_y(t)
     D  = polynomial_d(t)
@@ -150,41 +165,40 @@ def magnitudePoswinkel(t: float, pSinPS: float, pCosPS: float, longitude: float,
     X_p = polynomial_x_p(t)
     Y_p = polynomial_y_p(t)
 
-    # Koordinaten bezüglich der Fundamentalebene
-
+    # local coordinates in fundamental plane
     # https://de.wikipedia.org/wiki/Besselsche_Elemente
     # theta = ( \mu - 1.002738 * 360° / 86400 s * Delta_T ) + \Lambda
     # Sidereal day on Earth is approximately 86164.0905 s
     # 1.002738 = 86400 s / 86164.09 s
-    # theta = local hourangle corrected for TT
     #
+    # local coordinates in fundamental plane
     # H: theta, M: \mu, longitude = \Lambda
+    # theta = local hourangle corrected for TT
     H     = M - 360 / 86164.0905 * delta_t  + longitude
-    xi    = pCosPS * sin(H)
-    eta   = pSinPS * cos(D) - pCosPS * cos(H) * sin(D)
-    zeta  = pSinPS * sin(D) + pCosPS * cos(H) * cos(D)
-    # stündliche Änderungen
-    xi_p  = np.deg2rad(bessel_u[1] * pCosPS * cos(H))
+    xi    = rho_cos_phi_p * sin(H)
+    eta   = rho_sin_phi_p * cos(D) - rho_cos_phi_p * cos(H) * sin(D)
+    zeta  = rho_sin_phi_p * sin(D) + rho_cos_phi_p * cos(H) * cos(D)
+    # hourly changes, term for t=1
+    xi_p  = np.deg2rad(bessel_u[1] * rho_cos_phi_p * cos(H))
     eta_p = np.deg2rad(bessel_u[1] * xi * sin(D) - zeta * bessel_d[1])
 
-    U     = X - xi
-    V     = Y - eta
-    a     = X_p - xi_p
-    b     = Y_p - eta_p
-    L1S   = L1 - zeta * bessel_tanf1
-    L2S   = L2 - zeta * bessel_tanf2
-    n2    = a * a + b * b
+    U       = X - xi                    # coordinates relative to shadow axis
+    V       = Y - eta
+    a       = X_p - xi_p                # derivate - hourly changes for iteration
+    b       = Y_p - eta_p
+    L1_zeta = L1 - zeta * bessel_tanf1  # penumbra size at zeta
+    L2_zety = L2 - zeta * bessel_tanf2  # umbra size at zeta
 
-    return a, b, U, V, n2, L1S, L2S, D, H
+    return a, b, U, V, L1_zeta, L2_zety, D, H
 
 
 
 # Ergebnisse für die Ausgabe
 def ergebnisberechnung(t: float, U: float, V: float, L1S: float, L2S: float, D: float, H: float, latitude: float, delta_t: float) -> Tuple[float, float, float, float, float, float, float]:
     # Zeit des lokalen Maximums
-    TD = T0 + t
+    TD = bessel_T0 + t
     # Magnitude G
-    m = np.sqrt(U * U + V * V)
+    m = sqrt(U * U + V * V)
     G = (L1S - m) / (L1S + L2S)
     # Durchmesserverhältnis
     A = (L1S - L2S) / (L1S + L2S)
@@ -212,24 +226,49 @@ def ergebnisberechnung(t: float, U: float, V: float, L1S: float, L2S: float, D: 
 
 
 
-def bessel3(longitude: float, latitude: float, hoehe: float, delta_t: float) -> None:
-    ic(longitude, latitude, hoehe)
+def bessel3(delta_t: float) -> None:
+    longitude = Options.loc.lon.value
+    latitude  = Options.loc.lat.value
+    height    = Options.loc.height.value
+    ic(longitude, latitude, height)
 
-    # Hauptprogramm
-    t = 0
+    # geocentric from EarthLocation X, Y, Z
+    phi_p = atan( Options.loc.z.value / sqrt( sq(Options.loc.x.value) + sq(Options.loc.y.value) ) )
+    rho = sqrt( sq(Options.loc.x.value) + sq(Options.loc.y.value) + sq(Options.loc.z.value) ) / R_earth.value
+    rho_sin_phi_p = rho * sin(phi_p)
+    rho_cos_phi_p = rho * cos(phi_p)
+    ic(phi_p, rho_sin_phi_p, rho_cos_phi_p)
 
+    # geocentric coordinates
+    # https://de.wikipedia.org/wiki/Besselsche_Elemente
+    # e2 = 1 - sq(R_earth_pol) / sq(R_earth)
+    # C = 1 / sqrt(1 - e2 * sq(sin(latitude)))
+    # S = (1 - e2) * C
+    # phi_p = atan((R_earth.value*S + height) / (R_earth.value*C + height) * tan(latitude) )
+    # rho = (R_earth.value*C + height) * cos(latitude) / (R_earth.value * cos(phi_p))
+    # rho_sin_phi_p = rho * sin(phi_p)
+    # rho_cos_phi_p = rho * cos(phi_p)
+    # ic(e2, C, S, phi_p, rho, rho_sin_phi_p, rho_cos_phi_p)
+
+    # geocentric coordinates from prog95_3.py, slight difference 0.1° for phi_p (U)
     # rechtwinklige geozentrische Koordinaten
-    U = atan(0.99664719 * tan(latitude))
-    pSinPS = 0.99664719 * sin(U) + hoehe / 6378140 * sin(latitude)
-    pCosPS = cos(U) + hoehe / 6378140 * cos(latitude)
+    # ratio_earth = R_earth_pol.value / R_earth.value
+    # ratio_hoehe = height / R_earth.value
+    # ic(ratio_earth, ratio_hoehe)
+    # phi_p         = atan(ratio_earth * tan(latitude))
+    # rho_sin_phi_p = ratio_earth * sin(phi_p) + ratio_hoehe * sin(latitude)
+    # rho_cos_phi_p =               cos(phi_p) + ratio_hoehe * cos(latitude)
+    # ic(phi_p, rho_sin_phi_p, rho_cos_phi_p)
 
+    # Start at t = 0, relative to T0 from besselians
+    t = 0
     # 1) Ergebnisse zum Maximums-Zeitpunkt
     for i in range(5):  # 5 Iterationen genügen
-        a, b, U, V, n2, L1S, L2S, D, H = magnitudePoswinkel(t, pSinPS, pCosPS, longitude, delta_t)
+        a, b, U, V, L1S, L2S, D, H = calc_fundamental_plane(t, rho_sin_phi_p, rho_cos_phi_p, longitude, delta_t)
         # Zeitpunkt der maximalen Finsternis per Iteration
-        tm = -(U * a + V * b) / n2
+        tm = -(U * a + V * b) / (sq(a) + sq(b))
         t = t + tm
-        UTh, UTm, UTs, G, A, Zm, P = ergebnisberechnung(t, U, V, L1S, L2S, D, H, latitude, delta_t)
+    UTh, UTm, UTs, G, A, Zm, P = ergebnisberechnung(t, U, V, L1S, L2S, D, H, latitude, delta_t)
     print("Uhrzeit des Maximums:", UTh, "h", UTm, "m", UTs, "s UT")  # dynamische Zeit
     print("Magnitude: ", int(1000 * G) / 10, "%")
     print("Verhältnis Durchmesser Mond/Sonne: ", int(1000 * A) / 1000)
@@ -237,16 +276,16 @@ def bessel3(longitude: float, latitude: float, hoehe: float, delta_t: float) -> 
     print("Positionswinkel, bezogen auf Zenit: ", int(Zm+0.5), "Grad")
 
     # 2) Ergebnisse aller 10 Minuten
-    tmax = t
-    t = tmax - 3 
-    print("UT             Magnitude Nord-  Zenit- Posw.")
-    for i in range(480):  # 3 h aller Minuten
-        a, b, U, V, n2, L1S, L2S, D, H = magnitudePoswinkel(t, pSinPS, pCosPS, longitude, delta_t)
-        UTh, UTm, UTs, G, A, Zm, P = ergebnisberechnung(t, U, V, L1S, L2S, D, H, latitude, delta_t)
-        if G >= 0:
-            print("%2.0f h %02.0f m %02.0f s  %5.1f%%     %3.0f° %3.0f°" % (UTh, UTm, UTs, 100*G, P+0.5, Zm+0.5))
-            # print(UTh,"h", UTm,"m", UTs,"s  ", int(1000 * G) / 10, "%  ", int(P+0.5),"°  ", int(Zm+0.5), "°")
-        t = t + 1 / 60
+    # tmax = t
+    # t = tmax - 3 
+    # print("UT             Magnitude Nord-  Zenit- Posw.")
+    # for i in range(480):  # 3 h aller Minuten
+    #     a, b, U, V, n2, L1S, L2S, D, H = magnitudePoswinkel(t, pSinPS, pCosPS, longitude, delta_t)
+    #     UTh, UTm, UTs, G, A, Zm, P = ergebnisberechnung(t, U, V, L1S, L2S, D, H, latitude, delta_t)
+    #     if G >= 0:
+    #         print("%2.0f h %02.0f m %02.0f s  %5.1f%%     %3.0f° %3.0f°" % (UTh, UTm, UTs, 100*G, P+0.5, Zm+0.5))
+    #         # print(UTh,"h", UTm,"m", UTs,"s  ", int(1000 * G) / 10, "%  ", int(P+0.5),"°  ", int(Zm+0.5), "°")
+    #     t = t + 1 / 60
 
 #/ Uwe Pilz, Februar 2025
 
@@ -289,18 +328,24 @@ def main():
         time = Time(args.time, location=loc)
     elif not time:
         time = Time(Time.now(), location=loc)
-    # Delta T from astropy Time
-    # checked with https://www.iers.org/IERS/EN/DataProducts/tools/timescales/timescales.html
-    # slight difference in UT1
-    delta_t = ((time.tt.jd - time.ut1.jd) * u.day).to(u.s)
-    # approximation, see https://de.wikipedia.org/wiki/Delta_T
-    y = time.to_datetime().year + (time.to_datetime().month - 0.5) / 12
-    delta_t2 = 67.62 + 0.3645 * (y - 2015) + 0.0039755 * (y - 2015)**2
-    ic(time, delta_t, y, delta_t2)
+
+    # Delta T = TT - UT1
+    # https://de.wikipedia.org/wiki/Delta_T
+    # https://en.wikipedia.org/wiki/%CE%94T_(timekeeping)
+    # https://en.wikipedia.org/wiki/Leap_second
+    # https://www.iers.org/IERS/EN/DataProducts/tools/timescales/timescales.html
+    # (slight difference in UT1, UTC - UTC1 ~ 0.1 s)
+    # delta_t = ((time.tt.jd - time.ut1.jd) * u.day).to(u.s)
+    # using Delta T = TT - UTC here, as it's also used to convert TT -> UTC
+    delta_t = ((time.tt.jd - time.utc.jd) * u.day).to(u.s)
+    # alternate calculation (37 = leap seconds in 2025)
+    delta_t2 = ((time.utc.jd - time.ut1.jd) * u.day).to(u.s) + (37 + 32.184) * u.s
+    ic(time, delta_t, delta_t2)
+
     verbose(f"time UTC {time}, Delta T={delta_t:.2f}")
 
     # Run calculation for local circumstances
-    bessel3(loc.lon.value, loc.lat.value, loc.height.value, delta_t.value)
+    bessel3(delta_t.value)
 
 
 if __name__ == "__main__":
