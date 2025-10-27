bdc682cb5a2a356a499b7ec087e236dd7ca84d4a
phtnnz <git16@mj.m-j-s.net>
2025-10-27 21:40:08 +0100

    Changed comment to use e2_earth

--- bessel3.py
+++ bessel3.py
@@ -400,8 +400,7 @@ def geocentric(loc: EarthLocation) -> Tuple[float, float]:
     # # Var 2: geocentric coordinates
     # # https://de.wikipedia.org/wiki/Besselsche_Elemente
     # # See also [ESAA] (11.47-49)
-    # e2 = 1 - sq(R_earth_pol) / sq(R_earth)
-    # C = 1 / sqrt(1 - e2 * sq(sin(latitude)))
+    # C = 1 / sqrt(1 - e2_earth * sq(sin(latitude)))
     # S = (1 - e2) * C
     # phi_p = atan( (R_earth.value*S + height) / (R_earth.value*C + height) * tan(latitude) )
     # rho = (R_earth.value*C + height) * cos(latitude) / (R_earth.value * cos(phi_p))
