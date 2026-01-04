f5c807393ae31f83bbbc320036981dd4882a9dbc
phtnnz <git16@mj.m-j-s.net>
2026-01-04 18:46:43 +0100

    verbose() output moved to main() function, fake Moon_dist/Moon_alt for JPL

--- neoephem.py
+++ neoephem.py
@@ -54,7 +54,7 @@ from astroplan import Observer
 # Local modules
 from verbose import verbose, warning, error, message
 from astroutils import location_to_string, get_location     # ...
-from neoutils import Exposure, exposure_from_ephemeris, id_type_from_name
+from neoutils import Exposure, exposure_from_ephemeris
 from neoconfig import config
 
 DEFAULT_LOCATION = config.code
@@ -103,6 +103,7 @@ class EphemData:
     sort_time: Time
     ephem: Ephem
     times: EphemTimes
+    exposure: Exposure
 
 @dataclass
 class LocalCircumstances:
@@ -112,14 +113,16 @@ class LocalCircumstances:
     naut_dawn: Time         # nautical dawn
     epochs: dict            # epochs parameter for Ephem.from_mpc()/from_jpb()
 
+    def __str__(self):
+        return f"location {location_to_string(self.loc)}\nnautical twilight {self.naut_dusk.iso} / {self.naut_dawn.iso} ({self.naut_dusk.scale.upper()})"
+
 
 
 def get_ephem_mpc(objects: list, local: LocalCircumstances) -> dict[EphemData]:
     min_alt = config.min_alt
+    obj_data = {}
 
     for obj in objects:
-        verbose(f"object {obj}")
-
         try:
             # eph = Ephem.from_mpc(obj, location=loc, epochs=epochs)
             eph = Ephem.from_mpc(obj, location=local.loc, epochs=local.epochs, 
@@ -132,22 +135,21 @@ def get_ephem_mpc(objects: list, local: LocalCircumstances) -> dict[EphemData]:
             mag = eph["Mag"][0]
             mask = (eph["Alt"] > min_alt * u.deg) & (eph["Obstime"] > local.naut_dusk) & (eph["Obstime"] < local.naut_dawn)
             eph1 = eph[mask]
-            message.print_lines(eph1["Targetname", "Obstime", "RA", "DEC", "Mag", 
-                                        "Motion", "PA", "Az", "Alt", "Moon_dist", "Moon_alt"])
-
-            exp = exposure_from_ephemeris(eph, "Motion", mag)
-            message(exp)
+            exp = exposure_from_ephemeris(eph1, "Motion", mag)
+            data = EphemData(obj, None, eph1, None, exp)
+            obj_data[obj] = data
         except QueryError as e:
             warning(f"MPC ephemeris for {obj} failed")
+
+    return obj_data
 
 
 
 def get_ephem_jpl(objects: list, local: LocalCircumstances) -> dict[EphemData]:
     min_alt = config.min_alt
+    obj_data = {}
 
     for obj in objects:
-        verbose(f"object {obj}")
-
         eph = Ephem.from_horizons(obj, location=local.loc, epochs=local.epochs)
         # Compute total motion from RA/DEC rates
         eph["Motion"] = np.sqrt( np.square(eph["RA*cos(Dec)_rate"]) + np.square(eph["DEC_rate"]) )
@@ -158,11 +160,14 @@ def get_ephem_jpl(objects: list, local: LocalCircumstances) -> dict[EphemData]:
         mag = eph["Mag"][0]
         mask = (eph["Alt"] > min_alt * u.deg) & (eph["Obstime"] > local.naut_dusk) & (eph["Obstime"] < local.naut_dawn)
         eph1 = eph[mask]
-        ##FIXME: missing Moon_dist, Moon_alt in JPL ephemeris?
-        message.print_lines(eph1["Targetname", "Obstime", "RA", "DEC", "Mag", "Motion", "PA", "Az", "Alt"])
-
+        ##Quick hack: missing Moon_dist, Moon_alt in JPL ephemeris?
+        eph1["Moon_dist"] = 180 * u.degree
+        eph1["Moon_alt"]  = -90 * u.degree
         exp = exposure_from_ephemeris(eph, "Motion", mag)
-        message(exp)
+        data = EphemData(obj, None, eph1, None, exp)
+        obj_data[obj] = data
+
+    return obj_data
 
 
 
@@ -173,7 +178,6 @@ def get_local_circumstances(loc: EarthLocation) -> LocalCircumstances:
     # Observation times for upcoming night
     time = Time.now()
     ic(time)
-    verbose(f"time {time.iso} ({time.scale.upper()})")
 
     midnight = observer.midnight(time, which="next")
     twilight_evening = observer.twilight_evening_nautical(time, which="next")
@@ -189,8 +193,6 @@ def get_local_circumstances(loc: EarthLocation) -> LocalCircumstances:
     jd1 = day + rem
     midnight1 = Time(jd1, format="jd")
     ic(day, rem, midnight1.iso)
-    verbose(f"midnight {midnight.iso} / rounded {midnight1.iso} ({time.scale.upper()})")
-    verbose(f"nautical twilight {twilight_evening.iso} / {twilight_morning.iso} ({time.scale.upper()})")
     epochs = {"start":  midnight1 - 8 * u.hour,
               "step":   30 * u.min,
               "stop":   midnight1 + 9 * u.hour
@@ -226,7 +228,6 @@ def main():
     # Observer location and local circumstances
     loc = get_location(args.location if args.location else DEFAULT_LOCATION)
     ic(loc, loc.to_geodetic())
-    verbose(f"location {location_to_string(loc)}")
     local = get_local_circumstances(loc)
 
     # Objects
@@ -245,10 +246,18 @@ def main():
     if args.clear:
         MPC.clear_cache()    
 
+    verbose.print_lines(local)
+
     if args.jpl:
-        get_ephem_jpl(objects, local)
+        obj_data = get_ephem_jpl(objects, local)
     else:
-        get_ephem_mpc(objects, local)
+        obj_data = get_ephem_mpc(objects, local)
+
+    ic(obj_data)
+    for obj, data in obj_data.items():
+        verbose.print_lines(data.ephem["Targetname", "Obstime", "RA", "DEC", "Mag", 
+                                       "Motion", "PA", "Az", "Alt", "Moon_dist", "Moon_alt"])
+        verbose(data.exposure)
 
 
 
