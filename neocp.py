e4182717212fdf3ec3a5585252978b4ab8da0e86
phtnnz <git16@mj.m-j-s.net>
2026-02-07 18:05:09 +0100

    Added obs_planner_neocp, copy of process_objects, but using new data structure

--- neocp.py
+++ neocp.py
@@ -69,7 +69,7 @@ from neoplot import plot_objects
 from mpcneocp import mpc_query_neocp_ephemerides, mpc_query_neocp_list, parse_html_ephemerides, print_ephemerides
 from mpcneocp import parse_neocp_list_OLD, convert_text_ephemerides_OLD
 from mpcneocp import parse_neocp_list, obj_data_from_text_ephemerides, obj_data_add_neocp_list
-from neoclasses import Exposure
+from neoclasses import Exposure, Ephem, EphemData, EphemTimes
 from neoutils import obj_data_add_times, sort_obj_data, verbose_obj_data
 
 
@@ -319,6 +319,191 @@ def process_objects(ephemerides: dict, neocp_list: dict, pccp_list: dict, times_
         #   target=id, observation date (YYYY-MM-DD), time ut (HH:MM), ra, dec, exposure, number, filter (L),
         #   type, mag, nobs, arc, notseen, total
         csv_row = { "target": id,
+                    # "observation date": time_start_exp.strftime("%Y-%m-%d"),
+                    # "time ut": time_start_exp.strftime("%H:%M"),
+                    # "+0000" to enforce UTC
+                    "obstime": time_start_exp.strftime("%Y-%m-%d %H:%M:%S+0000"),
+                    "ra": float(ra.value),
+                    "dec": float(dec.value),
+                    "exposure": float(exp.value),
+                    "number": n_exp,
+                    "filter": "L",
+                    "start time": str(time_start_exp),
+                    "end time": str(time_end_exp),
+                    "type": type,
+                    "mag": float(mag.value),
+                    "nobs": nobs,
+                    "arc": float(arc.value),
+                    "notseen": float(notseen.value),
+                    "total": total
+                    }
+        ic(csv_row)
+        csv_rows.append(csv_row)
+
+        ##MJ: only 1st object for debugging
+        # return
+
+    # Return list of planned objects
+    message("-----------------------------------------------------------------------------------------------------------------------")
+    message(f"{len(objects)} object(s) planned: {" ".join(objects)}")
+
+    # Output to CSV file for nina-create-sequence2
+    if Options.csv:
+        verbose(f"planned objects for nina-create-sequence2: {Options.output}")
+        # csv_row is the last object, if any were found
+        if csv_row:
+            ##FIXME: improve csvoutput module to cover this usage
+            fieldnames = csv_row.keys()
+            ic(fieldnames)
+            if Options.output:
+                with open(Options.output, "w", newline="") as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                    writer.writerows(csv_rows)
+            else:
+                writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerows(csv_rows)
+        else:
+            warning("no objects, no CSV output")
+
+    # Return planned objects
+    return objects
+
+
+##NEW##
+def obs_planner_neocp(obj_data: dict[str, EphemData]) -> list:
+    """
+    New version of process_objects()
+    """
+    message("-----------------------------------------------------------------------------------------------------------------------")
+    message("             Score      MagV #Obs      Arc NotSeen  Time start ephemeris   / end ephemeris                  Max motion")
+    message("                                                    Time before            / after meridian              Moon distance")
+    message("                                                    Time start exposure    / end exposure")
+    message("                                                    # x Exp = total exposure time")
+    message("                                                    RA, DEC, Alt, Az")
+
+    csv_row = None
+    csv_rows = []
+    objects  = []
+    prev_time_end_exp = None
+
+    for obj, edata in obj_data.items():
+        # NEOCP/PCCP list data
+        type    = edata.neocp.type
+        score   = edata.neocp.score
+        mag     = edata.neocp.mag
+        nobs    = edata.neocp.nobs
+        arc     = edata.neocp.arc
+        notseen = edata.neocp.notseen
+        # Times
+        time_before, time_after       = edata.times.before, edata.times.after
+        time_first, time_last         = edata.times.start, edata.times.end
+        time_alt_first, time_alt_last = edata.times.alt_start, edata.times.alt_end
+        max_m = edata.motion
+        eph = edata.ephem
+
+        message("-----------------------------------------------------------------------------------------------------------------------")
+        message(f"{obj}  {type:5s} {score:3d}  {mag}  {nobs:3d}  {arc:5.2f}  {notseen:4.1f}  {time_first}/{time_last}  {max_m:5.1f}")
+
+        # Get exposure data
+        exposure = edata.exposure
+        if not exposure:                             # Object too fast
+            message(f"SKIPPED: object too fast (>{motion_limit():.1f})")
+            continue
+
+        n_exp = exposure.number
+        exp = exposure.single
+        total_exp = exposure.total
+        total_time = exposure.total_time
+        perc_of_required = exposure.percentage        
+        ic(n_exp, exp, total_exp, total_time, perc_of_required)
+
+        ##### Plan exposure start and end time ... #####
+        # Make sure to start after previous exposure
+        if prev_time_end_exp != None and time_first < prev_time_end_exp:
+            time_first = prev_time_end_exp
+        ic(prev_time_end_exp, time_first)
+
+        # 1st try: start at time_alt_first or time_first
+        time_start_exp = time_alt_first if time_alt_first >= time_first else time_first
+        time_end_exp   = time_start_exp + total_time
+        ic("1st try - alt first", time_start_exp, time_end_exp)
+        # ok if end <= before or start >= after and start >= previous
+
+        # 2nd try: start at time_before - total = before passing meridian
+        if time_end_exp > time_before and time_start_exp <= time_after:
+            time_start_exp = time_before - total_time
+            time_end_exp   = time_before
+            ic("2nd try - before meridian", time_start_exp, time_end_exp)
+        # ok if start >= first
+
+        # 3rd try: start at time_after = after passing meridian
+        if time_start_exp < time_first:
+            time_start_exp = time_after
+            time_end_exp   = time_after + total_time
+            ic("3rd try - after meridian", time_start_exp, time_end_exp)
+        # ok if end <= last
+        ic(time_start_exp, time_end_exp)
+
+        ##### Skip object for various reasons ... #####
+        # Skip, if below threshold for # obs
+        if nobs < config.min_n_obs:
+            message(f"SKIPPED: only {nobs} obs (< {config.min_n_obs})")
+            continue
+
+        # Skip, if not seen for more than threshold days
+        max_notseen = config.max_notseen * u.day
+        if notseen > max_notseen:
+            message(f"SKIPPED: not seen for {notseen:.1f} (> {max_notseen:.1f})")
+            continue
+
+        # Skip, if percentage of total exposure time is less than threshold
+        if perc_of_required < config.min_perc_required:
+            message(f"SKIPPED: only {perc_of_required:.0f}% of required total exposure time (< {config.min_perc_required}%)")
+            continue
+
+        # Skip, if arc is less than threshold
+        min_arc = config.min_arc * u.day
+        if arc < min_arc:
+            message(f"SKIPPED: arc {arc:.2f} too small (< {min_arc})")
+            continue
+
+        # Skip, if failed to allocate total_time
+        if time_end_exp > time_last:
+            message(f"SKIPPED: can't allocate exposure time {total_time:.2f} ({time_first} -- {time_last})")
+            continue
+
+        # Table row best matching time_start_exp
+        row = get_row_for_time(eph, time_start_exp)
+        ic(row)
+        moon_dist = row["moon_dist"]
+        ic(moon_dist)
+        # Skip, if moon distance is too small
+        min_moon_dist = config.min_moon_dist * u.degree
+        if moon_dist < min_moon_dist:
+            message(f"SKIPPED: moon distance {moon_dist:.0f} < {min_moon_dist:.0f}")
+            continue
+
+        ##### Good to go! #####
+        # Remember end of exposure
+        prev_time_end_exp = time_end_exp
+        # Append to list of planned objects
+        objects.append(obj)
+        ra, dec = row["ra"], row["dec"]
+        alt, az = row["alt"], row["az"]
+
+        message(f"                                                    {time_before}/{time_after}             {moon_dist:3.0f}")
+        message(f"                                                    {time_start_exp}/{time_end_exp}")
+        total = f"{n_exp} x {exp:2.0f} = {total_exp:3.1f} ({perc_of_required:.0f}%) / total {total_time:3.1f}"
+        message(f"                                                    {total}")
+        message(f"                                                    RA {ra:.4f}, DEC {dec:.4f}, Alt {alt:.0f}, Az {az:.0f}")
+
+        # CSV output:
+        #   start time, end time, 
+        #   target=id, observation date (YYYY-MM-DD), time ut (HH:MM), ra, dec, exposure, number, filter (L),
+        #   type, mag, nobs, arc, notseen, total
+        csv_row = { "target": obj,
                     # "observation date": time_start_exp.strftime("%Y-%m-%d"),
                     # "time ut": time_start_exp.strftime("%H:%M"),
                     # "+0000" to enforce UTC
