3e2357bf05e143d226d1c379129a883ff7e68ced
phtnnz <git16@mj.m-j-s.net>
2026-03-15 14:35:24 +0100

    Refactored to use functions from neoephem, import clean-up, version bumped to 0.3

--- sbephem.py
+++ sbephem.py
@@ -21,8 +21,10 @@
 #       Somewhat usable now ;-)
 # Version 0.2 / 2026-01-03
 #       Common column names for ephemeris tables
+# Version 0.3 / 2026-03-15
+#       Use functions from neoephem, with slightly different user interface
 
-VERSION     = "0.2 / 2026-01-03"
+VERSION     = "0.3 / 2026-03-15"
 AUTHOR      = "Martin Junius"
 NAME        = "sbephem"
 DESCRIPTION = "Ephemeris for solar system objects"
@@ -37,61 +39,19 @@ from icecream import ic
 ic.disable()
 
 # AstroPy
-from astropy.coordinates import AltAz, EarthLocation, SkyCoord
-# from astropy.coordinates import Angle
-# from astropy.coordinates import errors
-from astropy.time        import Time, TimeDelta
-from astropy.table       import Table
-# from astropy.units       import Quantity, Magnitude
+from astropy.time import Time
 import astropy.units as u
-import numpy as np
-
-from sbpy.data import Ephem
+from astroquery.mpc import MPC
 from sbpy.data import Obs
 from sbpy.data.core import QueryError
 
-from astroquery.mpc import MPC
-from astroquery.exceptions import EmptyResponseError, InvalidQueryError
-
-from astroplan import Observer
-
 # Local modules
-from verbose import verbose, warning, error, message
-from astroutils import location_to_string, get_location     # ...
+from verbose    import verbose, warning, error, message
+from neoconfig  import config
+from neoephem   import get_ephem_jpl, get_ephem_mpc, get_local_circumstances, get_dec_limits
 
 ##FIXME: use config
-DEFAULT_LOCATION = "M49"
-
-
-
-# Command line options
-class Options:
-    loc: EarthLocation = None       # -l --location
-
-
-
-def rename_columns_mpc(eph: Ephem) -> None:
-    """Rename MPC ephemeris table to common column names
-
-    Args:
-        table (Table): Ephemeris table
-    """
-    eph._table.rename_columns(("Date",    "Dec",      "V",             "Proper motion", "Direction", 
-                               "Azimuth", "Altitude", "Moon distance", "Moon altitude" ),
-                              ("Obstime", "DEC",      "Mag",           "Motion",        "PA",        
-                               "Az",      "Alt",      "Moon_dist",     "Moon_alt"      ))
-
-
-def rename_columns_jpl(eph: Ephem) -> None:
-    """Rename JPL ephemeris table to common column names
-
-    Args:
-        table (Table): Ephemeris table
-    """
-
-    mag_col = "Tmag" if "Tmag" in eph.field_names else "V"
-    eph.table.rename_columns(("targetname", "epoch",   "AZ", "EL",  mag_col, "velocityPA"),
-                             ("Targetname", "Obstime", "Az", "Alt", "Mag",   "PA" ))
+DEFAULT_LOCATION = config.code
 
 
 
@@ -109,8 +69,6 @@ def id_type_from_name(name: str) -> str:
             return id
     ## Default None or "asteroid designation"?
     return None
-
-
 
 
 
@@ -139,48 +97,31 @@ def main():
         verbose.set_prog(NAME)
         verbose.enable()
 
-    # Observer location
-    loc = get_location(args.location if args.location else DEFAULT_LOCATION)
-    ic(loc, loc.to_geodetic())
-    Options.loc = loc
-    verbose(f"location {location_to_string(loc)}")
+    # Observer location and local circumstances
+    local = get_local_circumstances(args.location if args.location else DEFAULT_LOCATION)
 
-    observer = Observer(location=loc, description=loc.info.name)
-    ic(observer)
+    # Override config DEC limits
+    min_dec, max_dec = get_dec_limits(local, config.min_alt*u.deg)
+    config.min_dec = int(min_dec.degree)
+    config.max_dec = int(max_dec.degree)
 
     # Observation time
     time = Time(args.time) if args.time else Time.now()
     ic(time)
     verbose(f"time {time.iso} ({time.scale.upper()})")
 
-    midnight = observer.midnight(time, which="next")
-    twilight_evening = observer.twilight_evening_nautical(time, which="next")
-    twilight_morning = observer.twilight_morning_nautical(time, which="next")
-    if twilight_evening > twilight_morning:
-        twilight_evening = observer.twilight_evening_nautical(time, which="previous")
-    ic(midnight.iso, twilight_evening.iso, twilight_morning.iso)
+    if not args.allnight:
+        local.epochs = {"start":  time,
+                        "step":   5 * u.min,
+                        "stop":   time + 1 * u.hour
+                        }
+        ic(local.epochs)
+    verbose.print_lines(local)
+    verbose("listing ephemeris for altitude>0 and after nautical dusk/before nautical dawn only!")
 
-    # Round midnight time to nearest 30 min
-    rem, day = np.modf(midnight.jd)
-    n_round = 24 * 2    # 24 h / 30 min
-    rem = round(rem*n_round) / n_round
-    jd1 = day + rem
-    midnight1 = Time(jd1, format="jd")
-    ic(day, rem, midnight1.iso)
-    verbose(f"midnight {midnight.iso} / rounded {midnight1.iso} ({time.scale.upper()})")
-    verbose(f"nautical twilight {twilight_evening.iso} / {twilight_morning.iso} ({time.scale.upper()})")
-
-    if args.allnight:
-        epochs = {"start":  midnight1 - 8 * u.hour,
-                  "step":   30 * u.min,
-                  "stop":   midnight1 + 9 * u.hour
-                  }
-    else:
-        epochs = {"start":  time,
-                  "step":   5 * u.min,
-                  "stop":   time + 1 * u.hour
-                  }
-    ic(epochs)
+    # Set min altitude for ephemeris to 0 deg
+    config.min_alt = 0
+    config.elev_min = 0
 
     # Objects
     objects = []
@@ -199,8 +140,6 @@ def main():
         MPC.clear_cache()    
 
     for obj in objects:
-        verbose(f"object {obj}")
-
         # # Object ephemeris
         # try:
         #     eph = MPC.get_ephemeris(obj, location=loc, start=time, step=5*u.min, number=12)
@@ -218,42 +157,19 @@ def main():
         #     warning(f"query MPC observations failed for {obj}!")
         #     obs = None
 
-        # Get ephemerides via sbpy
+        # Get ephemerides
         if args.jpl:
-            eph = Ephem.from_horizons(obj, location=loc, epochs=epochs)
-            # Compute total motion from RA/DEC rates
-            eph["Motion"] = np.sqrt( np.square(eph["RA*cos(Dec)_rate"]) + np.square(eph["DEC_rate"]) )
-            # Rename columns to common names
-            rename_columns_jpl(eph)
-            ic(eph.field_names)
-
-            mag = eph["Mag"][0]
-            ##FIXME: get min altitude from config
-            mask = (eph["Alt"] > 5 * u.deg) & (eph["Obstime"] > twilight_evening) & (eph["Obstime"] < twilight_morning)
-            eph1 = eph[mask]
-            message.print_lines(eph1["Targetname", "Obstime", "RA", "DEC", "Mag", "Motion", "PA", "Az", "Alt"])
-
+            obj_data = get_ephem_jpl(objects, local, type)
         else:
-            try:
-                # eph = Ephem.from_mpc(obj, location=loc, epochs=epochs)
-                eph = Ephem.from_mpc(obj, location=loc, epochs=epochs, 
-                                        ra_format={'sep': ':', 'unit': 'hourangle', 'precision': 1}, 
-                                        dec_format={'sep': ':', 'precision': 1} )
-                # Rename columns to common names
-                rename_columns_mpc(eph)
-                ic(eph.field_names)
+            obj_data = get_ephem_mpc(objects, local, type)
 
-                mag = eph["Mag"][0]
-                ##FIXME: get min altitude from config
-                mask = (eph["Alt"] > 5 * u.deg) & (eph["Obstime"] > twilight_evening) & (eph["Obstime"] < twilight_morning)
-                eph1 = eph[mask]
-                message.print_lines(eph1["Targetname", "Obstime", "RA", "DEC", "Mag", 
-                                         "Motion", "PA", "Az", "Alt", "Moon_dist", "Moon_alt"])
-            except QueryError as e:
-                warning(f"MPC ephemeris for {obj} failed")
+        for obj, edata in obj_data.items():
+            verbose.print_lines(edata.ephem["Targetname", "Obstime", "RA", "DEC", "Mag", 
+                                        "Motion", "PA", "Az", "Alt", "Moon_dist", "Moon_alt"])
 
-            try:
-                if args.obs:
+            ##CHECK: doesn't work properly
+            if args.obs:
+                try:
                     obs = Obs.from_mpc(obj, id_type=id_type_from_name(obj))
                     print(obs)
                     # # Handle masked entries
@@ -261,10 +177,10 @@ def main():
                     #     mag = obs["mag"][i].unmasked
                     #     if mag > Magnitude(0):
                     #         break
-            except QueryError as e:
-                warning(f"MPC observations for {obj} failed")
-            except ConnectionError as e:
-                warning(f"MPC request failed: {e}")
+                except QueryError as e:
+                    warning(f"MPC observations for {obj} failed")
+                except ConnectionError as e:
+                    warning(f"MPC request failed: {e}")
 
 
 
